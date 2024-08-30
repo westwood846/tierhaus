e70d5d282a25b4bc8b14c7f39c0383d56e3d6c39
westwood846 <PapstDonB@Googlemail.com>
2024-08-30 17:04:45 +0200

    Add @tanstack/router

---
+++ src/routes/index.lazy.tsx
@@ -0,0 +1,59 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+
+import { useQuery } from "@tanstack/react-query";
+import { graphql } from "../graphql";
+import { execute } from "../execute";
+
+const animalsQuery = graphql(`
+  query Animals {
+    animals {
+      name
+      id
+      species
+      born_at
+      weight_grams
+    }
+  }
+`);
+
+export const Route = createLazyFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  const { data } = useQuery({
+    queryKey: ["animals"],
+    queryFn: () => execute(animalsQuery),
+  });
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>ID</th>
+            <th>Species</th>
+            <th>Age</th>
+            <th>Weight (kg)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.animals.map((animal) => (
+            <tr key={animal.id}>
+              <td>{animal.name}</td>
+              <td>{animal.id}</td>
+              <td>{animal.species}</td>
+              <td>
+                {new Date().getFullYear() -
+                  new Date(animal.born_at).getFullYear()}{" "}
+                y/o
+              </td>
+              <td>{(animal.weight_grams / 1000).toFixed(2)} kg</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import codegen from "vite-plugin-graphql-codegen";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [codegen(), react()],
+  plugins: [TanStackRouterVite(), codegen(), react()],
 });
